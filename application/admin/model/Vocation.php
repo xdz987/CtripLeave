9876e725b79b2d0066573fd81c846367969f3286
xdz987 <1415187987@qq.com>
2019-04-02 12:55:26 +0800

    删除了数据表平均值和排名，简化了Vocation三个接口和对于model

--- application/admin/model/Vocation.php
+++ application/admin/model/Vocation.php
@@ -5,85 +5,70 @@ use think\Model;
 
 class Vocation extends Model {
 
-	//判断哪一个行业并进行计算
-	public function calculate($industry, $vocation, $exist) {
-		try {
-			//前缀
-			$prefix = config('database.prefix');
-			$tableName = $prefix . 'ranking';
+	//排名计算，数据整理
+	public function get_ranking($user_vocation) {
 
-			//当传递的exist不存在，vocation中没有用户插入的数据时，更新ranking排行榜
-			if (!$exist) {
-				//查询出用户对应的哪个行业
-				$rankRes = db('ranking')->find(['id' => $industry]);
+		//插入或更新user_vocation表
+		$old_user_vocation = $this->user_vocation_operate($user_vocation);
 
-				//以下进行排名ranking表插入前数据计算 增加数值+
-				$rankRes['id'] = $industry;
-				$rankRes['account_year_day'] = $rankRes['account_year_day'] + $vocation['vo_year_day']; //总人数的年假(暂时不需要，备用)
-				$rankRes['account_sum_day'] = $rankRes['account_sum_day'] + $vocation['vo_sum_day']; //总人数的年总假期
-				$rankRes['mem_sum'] = $rankRes['mem_sum'] + 1; //人数+1
-				$rankRes['average_sum_day'] = $rankRes['account_sum_day'] / $rankRes['mem_sum']; //年总假平均
-				db('ranking')->update($rankRes);
+		//数据表前缀
+		$prefix = config('database.prefix');
+		$industry_ranking_tablename = $prefix . 'industry_ranking';
+
+		//增加用户输入数据
+		db('user')->where('industry', $user_vocation['industry'])->setInc('mem_sum', 1);
+		db('user')->where('industry', $user_vocation['industry'])->setInc('account_year_day', $user_vocation['vo_year_day']);
+		db('user')->where('industry', $user_vocation['industry'])->setInc('account_sum_day', $user_vocation['vo_sum_day']);
+
+		if ($old_user_vocation) {
+			//删除用户输入数据
+			db('user')->where('industry', $user_vocation['industry'])->setDec('mem_sum', 1);
+			db('user')->where('industry', $user_vocation['industry'])->setDec('account_year_day', $old_user_vocation['vo_year_day']);
+			db('user')->where('industry', $user_vocation['industry'])->setDec('account_sum_day', $old_user_vocation['vo_sum_day']);
+		}
+
+		//查询所有行业数据
+		$industry_rankRes = Db::table($tableName)->limit(10)->select();
+		foreach ($industry_rankRes as $k => $v) {
+			//计算每个行业的平均数
+			if ($v['mem_sum']) {
+				$industry_rankRes[$k]['average_sum_day'] = $v['account_year_day'] / $v['mem_sum'];
 			} else {
-				//当用户想重新选择行业时，更新完vocation表后，ranking原先选的进行减-运算，再对新选的进行加+运算
-				//查询出用户对应的哪个行业
-				$rankRes = db('ranking')->find(['id' => $industry]);
-				//以下进行排名ranking表修改数据计算 增加数值+
-				$rankRes['id'] = $industry;
-				$rankRes['account_year_day'] = $rankRes['account_year_day'] + $vocation['vo_year_day']; //总人数的年假(暂时不需要，备用)
-				$rankRes['account_sum_day'] = $rankRes['account_sum_day'] + $vocation['vo_sum_day']; //总人数的年总假期
-				$rankRes['mem_sum'] = $rankRes['mem_sum'] + 1; //人数+1
-				$rankRes['average_sum_day'] = $rankRes['account_sum_day'] / $rankRes['mem_sum']; //年总假平均
-				db('ranking')->where('id', $industry)->update($rankRes);
-
-				//查询出用户之前选的老行业
-				$old_rankRes = db('ranking')->where('industry', $exist['industry'])->find();
-				//以下进行排名ranking表修改数据计算 减去数值-   3.31在ranking表中增加industry字段
-
-				$old_rankRes['account_year_day'] = $old_rankRes['account_year_day'] - $exist['vo_year_day']; //总人数的年假(暂时不需要，备用)
-				$old_rankRes['account_sum_day'] = $old_rankRes['account_sum_day'] - $exist['vo_sum_day']; //总人数的年总假期
-				$old_rankRes['mem_sum'] = $old_rankRes['mem_sum'] - 1; //人数+1
-				if (!($old_rankRes['mem_sum'] - 1)) {
-					$old_rankRes['average_sum_day'] = $old_rankRes['account_sum_day'] / $old_rankRes['mem_sum'];
-				} else {
-					$old_rankRes['average_sum_day'] = 0;
-				}
-				//年总假平均
-				db('ranking')->where('industry', $exist['industry'])->update($old_rankRes);
-
+				$industry_rankRes[$k]['average_sum_day'] = 0;
 			}
+		}
 
-			//查询老行业id
-			if ($exist) {
-				$old_industry = db('ranking')->field('id')->where('industry', $exist['industry'])->find();
-			}
-			//查询所有的数据排名
-			$in_rankRes = Db::table($tableName)->orderRaw('average_sum_day asc')->limit(10)->select();
-			foreach ($in_rankRes as $k => $v) {
-				$in_rankRes['all_rank'][$k + 1] = $v['id']; //所有行业排名 1-10
-				if ($v['id'] == $industry) {
-					$in_rank = $k + 1; //当前行业的排名
-				}
-				if ($exist) {
-					//更新后的老行业排名
-					if ($v['id'] == $old_industry['id']) {
-						$old_in_rank = $k + 1;
-					}
-				}
-			}
-			//赋值排名以便返回数据
-			$rankRes['in_rank'] = $in_rank; //当前行业排名
-			$rankRes['all_rank'] = $in_rankRes['all_rank']; //所有行业排名
+		//返回输入数组中某个单一列的值。
+		$average_sum_days = array_column($industry_rankRes, 'average_sum_day');
+		//对所有行业根据‘平均年总假’进行升序处理
+		array_multisort($average_sum_days, SORT_ASC, $industry_rankRes);
 
-			//更新当前行业的排名，当十个行业都被查询过一次，数据表的in_rank排名字段就全部都有数据
-			db('ranking')->where('id', $industry)->setField('in_rank', $in_rank); //更新新选择的行业排名
-			if ($exist) {
-				db('ranking')->where('id', $old_industry['id'])->setField('in_rank', $old_in_rank); //更新老行业排名
+		foreach ($industry_rankRes as $k => $v) {
+			$user_vocation['all_industry_rank'][$k + 1] = $v['industry']; //所有行业排名  例如第一名：1->A
+			if ($v['industry'] == $user_vocation['industry']) {
+				$user_vocation['average_sum_day'] = $industry_rankRes[$k]['average_sum_day']; //平均年假赋值
+				$user_vocation['user_industry_rank'] = $k + 1; //用户所选行业的的排名
 			}
-			return $rankRes;
+		}
 
-		} catch (Exception $e) {
-			echo "添加失败,请返回重新提交!";
+		//整理数据
+		unset($user_vocation['vo_sum_day']);
+		$user_vocation['nickname'] = db('user')->field('nickname')->where('id', $user_vocation['uid'])->find();
+
+		return $user_vocation;
+		//前端所需：全部行业排行榜	 1	用户姓名	1	所在行业	1	所在行业排名	1	所在行业平均年假	1	用户年假	1
+	}
+
+	//对user_vocation进行插入或者更新
+	public function user_vocation_operate($user_vocation) {
+
+		//查看user_vocation是否已插入基本信息，不存在则插入数据  存在则更新数据
+		$old_user_vocation = db('vocation')->where('uid', $user_vocation['uid'])->find();
+		if (!$old_user_vocation) {
+			db('vocation')->insert($user_vocation);
+		} else {
+			db('vocation')->where('uid', $user_vocation['uid'])->update($user_vocation);
+			return $old_user_vocation;
 		}
 	}
 }
